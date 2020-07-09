c4b6e6303ec62ad94ad7b8916d79228f94c6c367
bananana321 <68076512+bananana321@users.noreply.github.com>
2020-07-10 01:49:50 +0800

    Add files via upload

---
+++ run_analysis.R
@@ -0,0 +1,34 @@
+library(reshape2)
+# make sure to set working directory in the file 'UCI HAR Dataset'
+
+# get data from files
+X_train = read.table("train/X_train.txt")
+y_train = read.table("train/y_train.txt")
+subject_train = read.table("train/subject_train.txt")
+X_test = read.table("test/X_test.txt")
+y_test = read.table("test/y_test.txt")
+subject_test = read.table("test/subject_test.txt")
+features = read.table("features.txt")
+activity_labels = read.table("activity_labels.txt")
+
+# combine the test and train dataset
+X_data = rbind(X_train, X_test)
+y_data = rbind(y_train, y_test)
+subject_data = rbind(subject_train, subject_test)
+
+# extract feature with relevant word
+features_2ndCols = grep("-(mean|std).*", as.character(features[,2]))
+features_2ndColsNames = features[features_2ndCols, 2]
+
+# combine data
+X_data = X_data[features_2ndCols]
+allData = cbind(subject_data, y_data, X_data)
+
+#assign column name
+colnames(allData) = c("id", "Activity_Labels", features_2ndColsNames)
+allData$Activity_Labels = factor(allData$Activity_Labels, levels = activity_labels[,1], labels = activity_labels[,2])
+allData$Subject = as.factor(allData$id)
+
+meltedData = melt(allData, id = c("id", "Activity_Labels"))
+tidyData = dcast(meltedData, id + Activity_Labels ~ variable, mean)
+write.table(tidyData, "./tidy_data.txt", row.names = FALSE, quote = FALSE)
